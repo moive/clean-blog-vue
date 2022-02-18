3d4362c008036a030fb21406e836d95065c7690f
Moises Velasquez <mvelasquez@pictoric.com>
2022-02-18 17:43:45 -0500

    data in vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
+	<div v-html="text1"></div>
 	<router-view/>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { RichtextInstance, StoryblokResult } from 'storyblok-js-client';
+import { defineComponent, onMounted, ref, watch } from 'vue';
 import { storyapi } from './utils/api'
 
 export default defineComponent({
 	setup() {
+		let text1 = ref<string>('');
+
 		onMounted(async ()=>{
 
-			const data = await storyapi.get('cdn/stories', {
+			const { data } = await storyapi.get('cdn/stories', {
 				"token": process.env.VUE_APP_TOKEN,
 			});
 
-			console.log(data)
-		})
+			const t = await data.stories[0].content.body_post;
+			const t1 = await storyapi.richTextResolver.render(t);
+
+			text1.value = t1;
+		});
+		
+		
+		return { text1 }
 	},
 })
 </script>
